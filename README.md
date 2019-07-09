f48c32df8214d7ec2068544136be39a206074ef3
David Ackerman <enfipy@gmail.com>
2019-07-09 11:55:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<h2 align="center">Aunema website</h2>
+
+<p align="center">
+  <a href="https://cloud.drone.io/aunema/aunema.com"><img src="https://cloud.drone.io/api/badges/aunema/aunema.com/status.svg" alt="Build Status"></a>
+</p>
+
+Aunema website is under development. Feel free to contribute!
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
---
+++ src/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <h1 class="title">Aunema website</h1>
+  </div>
+</template>
+
+<style>
+.container {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+  padding-bottom: 15px;
+}
+</style>
